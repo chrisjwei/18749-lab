a2c1c1ae055de5a4fe82377bad0595eb94ea3cb9
Chris <cwei1994@gmail.com>
2017-09-19 13:12:00 -0400

    first commit

---
+++ edu/cmu/rds749/lab0/Proxy.java
@@ -0,0 +1,31 @@
+package edu.cmu.rds749.lab0;
+
+import Ice.Current;
+import Ice.Exception;
+import edu.cmu.rds749.common.AbstractProxy;
+
+/**
+ * Created by jiaqi on 8/28/16.
+ *
+ * Implements the Proxy.
+ */
+public class Proxy extends AbstractProxy
+{
+    public Proxy(String [] hostname, String [] port)
+    {
+        super(hostname, port);
+    }
+
+    public int readBalance(Current __current)
+    {
+        System.out.println("(In Proxy)");
+        return this.actualBankAccounts[0].readBalance();
+    }
+
+    public int changeBalance(int update, Current __current)
+    {
+        System.out.println("(In Proxy)");
+        return this.actualBankAccounts[0].changeBalance(update);
+    }
+
+}
